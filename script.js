e56998c3ab90fb651044bb9ab9d213b5597121e8
delcafune <durumm7@gmail.com>
2026-08-18 20:51:33 +0300

    Add saved cafes functionality

--- script.js
+++ script.js
@@ -52,7 +52,80 @@ async function showPosition(position) {
                 <h3>${cafeName}</h3>
                 <p>📍 ${cafeAddress}</p>
                 <p>⭐ ${cafeRating ?? "No rating yet"}</p>
+
+                <button class="save-btn" onclick='saveCafe(${JSON.stringify({
+                    name: cafeName,
+                    address: cafeAddress,
+                    rating: cafeRating,
+                    photo: photoURL
+                })})'>
+                    Save Cafe
+                </button>
             </div>
         `;
     });
+}
+
+function saveCafe(cafe) {
+        const savedCafes = 
+            JSON.parse(localStorage.getItem("savedCafes")) || [];
+
+        const alreadySaved = savedCafes.some(savedCafe =>
+            savedCafe.name === cafe.name &&
+            savedCafe.address === cafe.address
+        );
+
+        if (alreadySaved) {
+            alert(`${cafe.name} is already saved!`);
+            return;
+        }
+
+        savedCafes.push(cafe);
+
+        localStorage.setItem("savedCafes", JSON.stringify(savedCafes));
+
+        alert(`${cafe.name} saved!`);
+}
+
+function showSaved() {
+    const savedCafes =
+        JSON.parse(localStorage.getItem("savedCafes")) || [];
+    
+    const cards = document.querySelector(".cards");
+
+    cards.innerHTML = "";
+
+    savedCafes.forEach(cafe => {
+        cards.innerHTML += `
+            <div class="card">
+                ${cafe.photo
+                    ? `<img src="${cafe.photo}" alt="${cafe.name}" class="cafe-photo">`
+                    : `<div class="no-photo">☕</div>`
+                }
+
+                <h3>${cafe.name}</h3>
+                <p>📍 ${cafe.address}</p>
+                <p>⭐ ${cafe.rating ?? "No rating yet"}</p>
+
+                <button class="remove-btn" onclick='removeCafe(${JSON.stringify(cafe)})'>
+                    Remove Cafe
+                </button>
+            </div>
+        `;
+    });
+}
+function removeCafe(cafe) {
+    let savedCafes =
+        JSON.parse(localStorage.getItem("savedCafes")) || [];
+
+    savedCafes = savedCafes.filter(savedCafe =>
+        !(
+            savedCafe.name === cafe.name &&
+            savedCafe.address === cafe.address
+        )
+    );
+
+    localStorage.setItem("savedCafes", JSON.stringify(savedCafes));
+
+    showSaved();
 }
